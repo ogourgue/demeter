fece525cc7e8bee694acd9459b8c5e0b7012c1b2
Olivier Gourgue <ogourgue@gmail.com>
2022-09-21 12:24:50 +0200

    Create test for emport/export of cellular automaton state files
    This is only for the first version of cellular automaton output files.
    
    New version to be implemented later (new tests in this file will be added).

---
+++ tests/test_ca_import_export.py
@@ -0,0 +1,65 @@
+import os
+import numpy as np
+from demeter import cellular_automaton
+
+# Grid parameters.
+x0 = 0
+y0 = 0
+nx = 10
+ny = 10
+dx = 1
+
+
+######################################
+# Binary file version 1, deprecated. #
+######################################
+
+# Create cellular automaton.
+ca = cellular_automaton.CellularAutomaton(x0, y0, nx, ny, dx)
+
+# Append initial time and state.
+ca.append_times(0)
+ca.append_state(np.zeros((nx, ny), dtype = int))
+
+# Vegetation parameters.
+p_est = .01
+p_die = 0
+r_exp = 0
+nt = 10
+
+# Create cellular automaton.
+ca = cellular_automaton.CellularAutomaton(x0, y0, nx, ny, dx)
+
+# Append initial time and state.
+ca.append_times(0)
+ca.append_state(np.zeros((nx, ny), dtype = int))
+
+# Update probabilities.
+ca.update_probabilities(p_est, p_die, r_exp)
+
+# Loop over years.
+for i in range(nt):
+    ca.run(1)
+    ca.append_times(i + 1)
+
+# Export all time steps.
+ca.export('test_all.bin')
+
+# Export last time step.
+ca.export('test_last.bin', step = -1)
+
+# Test importing all time steps.
+test = cellular_automaton.CellularAutomaton.from_file('test_all.bin')
+assert np.array_equal(ca.state, test.state)
+
+# Test importing last time step from all-time-step file.
+test = cellular_automaton.CellularAutomaton.from_file('test_all.bin', step = -1)
+assert np.array_equal(ca.state[-1, :, :], test.state[0, :, :])
+
+# Test importing last time step from last-time-step file.
+test = cellular_automaton.CellularAutomaton.from_file('test_last.bin')
+assert np.array_equal(ca.state[-1, :, :], test.state[0, :, :])
+
+# Delete files.
+os.remove('test_all.bin')
+os.remove('test_last.bin')
