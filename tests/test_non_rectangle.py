528e8bf2bcc06c28603515ca4ac12dd44d86d1d8
Olivier Gourgue <ogourgue@gmail.com>
2023-02-15 16:03:11 +0100

    Test excluding cellular automaton cells outside Telemac grid (no new development needed).

--- tests/test_non_rectangle.py
+++ tests/test_non_rectangle.py
@@ -35,17 +35,48 @@ ny = int((np.max(tel.y) - np.min(tel.y)) / DX)
 # Create cellular automaton.
 ca = cellular_automaton.CellularAutomaton(x0, y0, nx, ny, DX)
 
+# Append initial time and state.
+ca.append_times(0)
+ca.append_state(np.zeros((nx, ny)))
+
 
 ##################################
 # Telemac to cellular automaton. #
 ##################################
 
-# Interpolate x-coordinates.
-x = tel2ca.interpolation(tel.x, tel.y, tel.x, tel.tri, ca.x, ca.y,
-                         nproc = NPROC)
+# Determine which cellular automaton cells are inside Telemac grid.
+inside = tel2ca.interpolation(tel.x, tel.y, np.ones(tel.npoin), tel.tri, ca.x, ca.y, nproc = NPROC)
 
 # Test.
-assert np.mean(np.abs(ca.x - x[:, -1])) < 1e-9
+assert np.sum(np.isnan(inside)) == 3601
+
+
+###########################
+# Run cellular automaton. #
+###########################
+
+# Initial state.
+state = np.zeros((nx, ny))
+state[np.isnan(inside)] = -1
+
+# Append initial time and state.
+ca.append_times(0)
+ca.append_state(state)
+
+# Update probabilities.
+ca.update_probabilities(.5, 0, 0)
+
+# Run cellular automaton.
+ca.run(1, nproc = NPROC)
+
+# Test finale state.
+state = ca.state[-1, :, :]
+assert np.mean(state[np.isnan(inside)])
+
+
+#############
+# Finalize. #
+#############
 
 # Delete intermediate files.
 shutil.rmtree('./tmp_tel2ca')
