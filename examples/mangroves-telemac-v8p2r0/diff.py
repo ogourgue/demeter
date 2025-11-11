efe5186debfb4b5a9c7112ab6a0370ff089d0e6f
Olivier Gourgue <ogourgue@naturalsciences.be>
2025-11-11 18:33:36 +0100

    Update diff.py of examples to only account for last time step for plotting vmin and vmax.

--- examples/mangroves-telemac-v8p2r0/diff.py
+++ examples/mangroves-telemac-v8p2r0/diff.py
@@ -28,22 +28,22 @@ if os.path.isdir('./Diff'):
 os.mkdir('./Diff')
 
 # Import Telemac output file.
-vnames = ['bottom', 'hydroperiod', 'ebs impulse', 'coverage']
+vnames = ['bottom', 'hydroperiod', 'ebs impulse', 'coverage', 'age']
 out = telemac.Telemac('./out_tel.slf', vnames = vnames)
 ref = telemac.Telemac('./ref_tel.slf', vnames = vnames)
 x = out.x
 y = out.y
 tri = out.ikle - 1
-b_diff = out.b - ref.b
-th_diff = (out.th - ref.th) / (N * (12 * 60 + 25) * 60) * 100
-jb_diff = (out.jb - ref.jb) / (N * (12 * 60 + 25) * 60 * TAUC) * 100
-cov_diff = (out.cov - ref.cov) * 100
-age_diff = (out.age - ref.age)
+b_diff = out.b[-1, :] - ref.b[-1, :]
+th_diff = (out.th[-1, :] - ref.th[-1, :]) / (N * (12 * 60 + 25) * 60) * 100
+jb_diff = (out.jb[-1, :] - ref.jb[-1, :]) / (N * (12 * 60 + 25) * 60 * TAUC) * 100
+cov_diff = (out.cov[-1, :] - ref.cov[-1, :]) * 100
+age_diff = (out.age[-1, :] - ref.age[-1, :])
 
 # Figures.
 plt.figure()
 vmax = np.max(np.abs(b_diff))
-plt.tripcolor(x, y, tri, b_diff[-1, :], cmap = 'RdBu', vmin = -vmax, vmax = vmax)
+plt.tripcolor(x, y, tri, b_diff, cmap = 'RdBu', vmin = -vmax, vmax = vmax)
 plt.axvline(x = -1000, color = 'k', linestyle = '--')
 plt.axvline(x = 0, color = 'k', linestyle = '--')
 plt.axis('scaled')
@@ -58,7 +58,7 @@ plt.close()
 
 plt.figure()
 vmax = np.max(np.abs(th_diff))
-plt.tripcolor(x, y, tri, th_diff[-1, :], cmap = 'RdBu', vmin = -vmax, vmax = vmax)
+plt.tripcolor(x, y, tri, th_diff, cmap = 'RdBu', vmin = -vmax, vmax = vmax)
 plt.axvline(x = -1000, color = 'k', linestyle = '--')
 plt.axvline(x = 0, color = 'k', linestyle = '--')
 plt.axis('scaled')
@@ -73,7 +73,7 @@ plt.close()
 
 plt.figure()
 vmax = np.max(np.abs(jb_diff))
-plt.tripcolor(x, y, tri, jb_diff[-1, :], cmap = 'RdBu', vmin = -vmax, vmax = vmax)
+plt.tripcolor(x, y, tri, jb_diff, cmap = 'RdBu', vmin = -vmax, vmax = vmax)
 plt.axvline(x = -1000, color = 'k', linestyle = '--')
 plt.axvline(x = 0, color = 'k', linestyle = '--')
 plt.axis('scaled')
@@ -88,7 +88,7 @@ plt.close()
 
 plt.figure()
 vmax = np.max(np.abs(cov_diff))
-plt.tripcolor(x, y, tri, cov_diff[-1, :], cmap = 'RdBu', vmin = -vmax, vmax = vmax)
+plt.tripcolor(x, y, tri, cov_diff, cmap = 'RdBu', vmin = -vmax, vmax = vmax)
 plt.axvline(x = -1000, color = 'k', linestyle = '--')
 plt.axvline(x = 0, color = 'k', linestyle = '--')
 plt.axis('scaled')
@@ -103,7 +103,7 @@ plt.close()
 
 plt.figure()
 vmax = np.max(np.abs(age_diff))
-plt.tripcolor(x, y, tri, age_diff[-1, :], cmap = 'RdBu', vmin = -vmax, vmax = vmax)
+plt.tripcolor(x, y, tri, age_diff, cmap = 'RdBu', vmin = -vmax, vmax = vmax)
 plt.axvline(x = -1000, color = 'k', linestyle = '--')
 plt.axvline(x = 0, color = 'k', linestyle = '--')
 plt.axis('scaled')
